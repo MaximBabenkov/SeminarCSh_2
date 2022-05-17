d96fb9b4297e2a03dd9417fa6100971d9ffed105
MaximBabenkov <lostbass@mail.ru>
2022-05-17 10:53:17 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить пятизначное число на палиндром
+Console.WriteLine("Enter your five-digit number");
+int a = int.Parse(Console.ReadLine()??"0");
+if(a/10000==a%10&&(a/1000)%10==(a/10)%10)
+{
+    Console.WriteLine("Your number is a palindrome");
+}
+else
+{
+    Console.WriteLine("Your number is not a palindrome");
+}
