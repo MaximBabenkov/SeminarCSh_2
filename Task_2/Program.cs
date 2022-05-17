e58c2badc6b954157e6487a1979101bd99a58b93
MaximBabenkov <lostbass@mail.ru>
2022-05-17 10:42:40 +0300

    Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Определить номер четверти, в которой находится точка с координатами x и y, 
+//  x и y не равны 0
+Console.WriteLine("Enter your x coordinate");
+int x = int.Parse(Console.ReadLine()??"0");
+Console.WriteLine("Enter your y coordinate");
+int y = int.Parse(Console.ReadLine()??"0");
+if(x>0&&y>0) Console.WriteLine("The point locates in quarter 1");
+else if(x<0&&y>0) Console.WriteLine("The point locates in quarter 2");
+else if(x<0&&y<0) Console.WriteLine("The point locates in quarter 3");
+else if(x>0&&y<0) Console.WriteLine("The point locates in quarter 4");
