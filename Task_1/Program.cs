ba7a69f17ff4b0be828a1fbbf3b5f92299b0e24b
MaximBabenkov <lostbass@mail.ru>
2022-05-17 10:21:43 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,11 @@
+﻿// Проверить - кратно ли введенное число одновременно 7 и 23
+Console.WriteLine("Enter your number");
+int a = int.Parse(Console.ReadLine()??"0");
+if(a%7==0&&a%23==0)
+{
+    Console.WriteLine("Your number is aliquot to 7 and 23 at the same time");
+}
+else
+{
+    Console.WriteLine("Your number is not aliquot to 7 and 23 at the same time");
+}
